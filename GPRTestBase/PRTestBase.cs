fe0da60a528f812c1b0699839367a209e2cf5618
CyberFicus <cyberficus9997@gmail.com>
2025-05-22 23:39:53 +0300

    PRTestBase обновление описаний

--- GPRTestBase/PRTestBase.cs
+++ GPRTestBase/PRTestBase.cs
@@ -3,14 +3,14 @@
 namespace PRTestBase
 {
     /// <summary>
-    /// Provides inheritable base functionality for Pesudo Random Tests
+    /// Provides inheritable base functionality for pseudo random tests
     /// <para>ApplySeed is to be used before pseudo random test every test for their results to be repeatable</para>
-    /// <para>PRTestIterations is to define how many iterations of testing RunIterations should run</para>
+    /// <para>PRTestIterations is to define how many iterations of testing should be in RunIterations</para>
     /// </summary>
     public class PRTestBase
     {
         /// <summary>
-        /// To be seeded with constant and used in all tests with pseudo random input
+        /// To be seeded with a constant and used in all tests with pseudo random input
         /// </summary>
         public static Random SharedRandom { get; protected set; }
         /// <summary>
@@ -31,19 +31,19 @@ namespace PRTestBase
         
         public delegate void Iteration(object?[]? args);
         /// <summary>
-        /// Runs iteration of pseudo random test defined number of times
+        /// Runs iteration of pseudo random test defined number of times. Allows to pass arguments
         /// </summary>
-        /// <param name="args">Set of arguments passed to iteration</param>
-        public virtual void RunIterations(Iteration iteration, object?[]? args = null)
+        /// <param name="arguments">Set of arguments passed to iteration</param>
+        public virtual void RunIterations(Iteration iteration, object?[]? arguments = null)
         {
             for (int i = 0; i < PRTestIterations; i++)
             {
-                iteration(args);
+                iteration(arguments);
             }
         }
         public delegate void IterationNoArgs();
         /// <summary>
-        /// Runs iteration of pseudo random test defined number of times
+        /// Runs iteration of pseudo random test defined number of times with no arguments.
         /// </summary>
         public virtual void RunIterations(IterationNoArgs iteration)
         {
